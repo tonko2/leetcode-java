94e206ae6811a3278805b1cab9468c5336be37d7
Yuya Kusaka <10cherry07@gmail.com>
2025-07-05 21:04:35 +0900

    Sort practice

---
+++ playground/BubbleSort.java
@@ -0,0 +1,24 @@
+package playground;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 8, 12, 3};
+        bubbleSort(numbers);
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
